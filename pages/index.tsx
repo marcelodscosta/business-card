003c4eb4c8b43adf3e885558bb808e7eb23dd09a
marcelodscosta <81534727+marcelodscosta@users.noreply.github.com>
2023-07-04 13:12:08 -0300

    applying global styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,3 @@
-import { styled } from '../styles'
-
-const Button = styled('button', {
-  backgroundColor: '$primary',
-  borderRadius: 8,
-  padding: '8px 6px',
-  border: 0,
-
-  '&:hover': {
-    filter: 'brightness(0.8)',
-  },
-})
-
-const H1 = styled('h1', {
-  fontFamily: 'roboto',
-  fontWeight: 'normal',
-})
-
 export default function Home() {
-  return (
-    <div>
-      <H1>Entendo as fontes</H1>
-      <Button onClick={() => alert('Teste')}>Hello World</Button>
-    </div>
-  )
+  return <div>Testando fonte</div>
 }
