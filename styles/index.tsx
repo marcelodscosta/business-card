fab33d131ca5fa0ad43c4d55ea63333a0333e4c7
marcelodscosta <81534727+marcelodscosta@users.noreply.github.com>
2023-07-04 12:56:03 -0300

    configuring stitches

---
+++ styles/index.tsx
@@ -0,0 +1,18 @@
+import { createStitches } from '@stitches/react'
+
+export const {
+  config,
+  createTheme,
+  css,
+  getCssText,
+  globalCss,
+  keyframes,
+  styled,
+  theme,
+} = createStitches({
+  theme: {
+    colors: {
+      primary: '#505050',
+    },
+  },
+})
