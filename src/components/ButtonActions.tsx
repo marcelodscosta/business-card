7d6bea4cca077bb4ded88e711056f0caa0d018a3
marcelodscosta <81534727+marcelodscosta@users.noreply.github.com>
2023-07-05 15:58:05 -0300

    extend button properties

--- src/components/ButtonActions.tsx
+++ src/components/ButtonActions.tsx
@@ -1,14 +1,15 @@
 import Image from 'next/image'
+import { ButtonHTMLAttributes } from 'react'
 import { Button, Icon, Title } from '../styles/ButtonActions'
 
 type ButtonActionProps = {
   icon?: any
   title: string
-}
+} & ButtonHTMLAttributes<HTMLButtonElement>
 
-export const ButtonActions = ({ icon, title }: ButtonActionProps) => {
+export const ButtonActions = ({ icon, title, ...rest }: ButtonActionProps) => {
   return (
-    <Button>
+    <Button {...rest}>
       <Icon>
         <Image src={icon} alt="contact" />
       </Icon>
--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -1,14 +1,15 @@
 import Image from 'next/image'
+import { ButtonHTMLAttributes } from 'react'
 import { Button } from '../styles/Button'
 
 type IconButtonProps = {
   title: string
   icon: string
-}
+} & ButtonHTMLAttributes<HTMLButtonElement>
 
-export const IconButton = ({ title, icon }: IconButtonProps) => {
+export const IconButton = ({ title, icon, ...rest }: IconButtonProps) => {
   return (
-    <Button>
+    <Button {...rest}>
       <Image src={icon} alt="icon" style={{ marginRight: 12 }} />
       {title}
     </Button>
